af6f6f4c394a15c3f29985ce3768f8fcf4392eea
SEBOMBI <95011515+SEBOMBI@users.noreply.github.com>
2022-03-03 21:45:06 +0900

    component 적용

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="menu">
+    <a v-for="(menus,id) in menu" :key="id">{{ menus }}</a>
   </div>
+
+<!--  <div v-for="(real,id) in (estates)" :key="id">
+    <div class="black-bg"  v-if="real.modal==true" >
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지내용임</p>
+        <button @click="real.modal=false">닫기</button>
+      </div>
+    </div>
+      <img src="../assets/estates/room1.jpg" class="room-img"/>
+      <h4 :style="h4Style" @click="real.modal=true">{{ real.name }}</h4>
+      <p>{{ price[id] }} 천만원</p>
+      <button @click="increase(id)">허위매물신고</button>
+      <span>신고수 : {{ real.report }}</span>
+  </div>-->
+
+    <!--MODAL-->
+    <div class="black-bg"  v-if="modal===true" >
+      <div class="white-bg">
+        <img :src="oneRooms[pickedModal].image" class="room-img"/>
+        <h4>{{oneRooms[pickedModal].title}}</h4>
+        <p>{{ oneRooms[pickedModal].content }}</p>
+        <h3> 가격 : {{oneRooms[pickedModal].price}} </h3>
+        <DiscountComponent/>
+        <button @click="modal=false">닫기</button>
+      </div>
+    </div>
+
+  <div v-for="(estate,id) in (oneRooms)" :key="id">
+    <img :src="estate.image" class="room-img"/>
+    <h4 :style="h4Style" @click="modal=true; pickedModal=id; ">{{ estate.title }}</h4>
+  </div>
+
 </template>
 
+
 <script>
+
+import oneRoomData from '../assets/oneRoomData/oneRooms';
+import DiscountComponent from "@/components/DiscountComponent";
+
 export default {
   name: 'HelloWorld',
+  components: {DiscountComponent},
   props: {
     msg: String
+  },
+  data() {
+    return {
+      DiscountComponent,
+      pickedModal : 0,
+      modal : false,
+      oneRooms : oneRoomData,
+      price: [3, 2, 4],
+      h4Style: 'color : blue',
+      estates: [
+        {name: '역삼동원룸', report: 0, modal: false},
+        {name: '천호동원룸', report: 0, modal: false},
+        {name: '마포구원룸', report: 0, modal: false}],
+      menu: ['HOME', 'PRODUCT', 'ABOUT']
+    }
+  },
+  methods: {
+    increase(id) {
+      this.estates[id].report++
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+body {
+  margin: 0;
 }
-a {
-  color: #42b983;
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+  top: 1px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img{
+  width: 50%;
+  margin-top: 5%;
 }
 </style>
